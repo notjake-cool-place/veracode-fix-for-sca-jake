88922e921496b0a3554222cd625824b52cb87d65
notjake <jhyland@veracode.com>
2026-03-23 15:43:20 -0400

    init

---
+++ src/index.js
@@ -0,0 +1,63 @@
+const core = require('@actions/core');
+
+const setupAstGrep = require('./setup-ast-grep');
+const runFixSca = require('./run-fix-sca');
+const createPr = require('./create-pr');
+const uploadPrComment = require('./upload-pr-comment');
+
+async function main() {
+  try {
+    // Get inputs
+    const githubToken = core.getInput('github-token');
+    const repository = core.getInput('repository');
+    const branch = core.getInput('branch');
+    const githubApiUrl = core.getInput('github-api-url');
+    const prNumber = core.getInput('pr-number');
+    const fixScaParams = core.getInput('fix-sca-params');
+
+    const workspaceDir = process.env.GITHUB_WORKSPACE;
+    const actionPath = `${__dirname}/..`
+
+    core.info('Starting Veracode Fix for SCA action...');
+
+    // Setup ast-grep
+    core.info('Setting up ast-grep...');
+    await setupAstGrep(actionPath);
+
+    // Run Fix for SCA
+    core.info('Running Fix for SCA...');
+    const fixScaOutput = await runFixSca(workspaceDir, actionPath, fixScaParams);
+    
+    if (!fixScaOutput.hasChanges) {
+      core.info('No changes detected. Skipping PR creation.');
+      return;
+    }
+
+    // Create Pull Request
+    core.info('Creating pull request...');
+    const prCreateOutput = await createPr(
+      workspaceDir,
+      repository,
+      branch,
+      githubToken,
+      githubApiUrl
+    );
+
+    // Post PR comment on original PR
+    core.info('Posting comment on original PR...');
+    await uploadPrComment(
+      workspaceDir,
+      repository,
+      prNumber,
+      githubToken,
+      githubApiUrl
+    );
+
+    core.info('Veracode Fix for SCA action completed successfully.');
+  } catch (error) {
+    core.setFailed(error.message);
+    process.exit(1);
+  }
+}
+
+main();
